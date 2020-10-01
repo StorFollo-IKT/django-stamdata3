42d5c6d78aab88acff789dd16c9274fd2c5f9159
datagutten <datagutten@datagutten.net>
2020-10-01 15:12:23 +0200

    Load resources for all companies

--- employee_info/management/commands/load_resources.py
+++ employee_info/management/commands/load_resources.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.core.management.base import BaseCommand
 
 from employee_info.load_data.load_resources import LoadResources
+from employee_info.models import Company
 
 now = datetime.now()
 
@@ -12,8 +13,14 @@ class Command(BaseCommand):
         parser.add_argument('company', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        customer = options['company'][0]
-        file = '/home/datagrunnlag/Stamdata3_teis_%s.xml' % customer
+        company = options['company'][0]
+        if company == 'all':
+            companies = Company.objects.all()
+        else:
+            companies = [company]
 
-        load = LoadResources(file, customer)
-        load.load()
+        for company in companies:
+            file = '/home/datagrunnlag/Stamdata3_teis_%s.xml' % company.companyCode
+
+            load = LoadResources(file, company.companyCode)
+            load.load()
