dcb8e4549f0bc6455e11c3d77c23c12eddd999b1
datagutten <datagutten@datagutten.net>
2021-01-04 14:37:13 +0100

    Allow null value for description fields

--- employee_info/models.py
+++ employee_info/models.py
@@ -74,7 +74,7 @@ class CostCenter(models.Model):
 class WorkPlace(models.Model):
     company = models.ForeignKey(Company, on_delete=models.PROTECT, verbose_name='firma', related_name='work_places')
     value = models.CharField('verdi', max_length=4)
-    description = models.CharField('beskrivelse', max_length=200)
+    description = models.CharField('beskrivelse', max_length=200, null=True, blank=True)
 
     def __str__(self):
         return '%s %s (%s)' % (self.value, self.description, self.company.companyCode)
@@ -88,7 +88,7 @@ class WorkPlace(models.Model):
 class Function(models.Model):
     company = models.ForeignKey(Company, on_delete=models.PROTECT, verbose_name='firma', related_name='functions')
     value = models.CharField('verdi', max_length=4)
-    description = models.CharField('beskrivelse', max_length=200)
+    description = models.CharField('beskrivelse', max_length=200, null=True, blank=True)
 
     def __str__(self):
         return '%s %s (%s)' % (self.value, self.description, self.company.companyCode)
@@ -127,7 +127,7 @@ class Employment(models.Model):
     postId = models.IntegerField()
     postIdDescription = models.CharField(max_length=200)
     postCode = models.IntegerField('stillingskode')
-    postCodeDescription = models.CharField(max_length=200)
+    postCodeDescription = models.CharField(max_length=200, null=True, blank=True)
 
     dateFrom = models.DateField('dato fra')
     dateTo = models.DateField('dato til')
